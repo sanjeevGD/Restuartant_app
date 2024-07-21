d21c8ffdbf267d0edaaaeb1dd3eeac67c822afe6
SanjeevGD <saanjusp@gmail.com>
2024-07-21 19:42:27 +0530

    resturant app

---
+++ src/components/Basics/menuApi.js
@@ -0,0 +1,193 @@
+const Menu = [
+    {
+      id: 1,
+      image: "../images/maggi.jpg",
+      name: "maggi",
+      category: "breakfast",
+      price: "40₹",
+      description:
+        "I love Maggi realy oo yues  Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+  
+    {
+      id: 2,
+      image: "../images/Biryani.jpg",
+      name: "chicken Biryani",
+      category: "dinner",
+      price: "180₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 3,
+      image: "../images/allupakoida.jpg",
+      name: "allu pakkoada",
+      category: "breakfast",
+      price: "25₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 4,
+      image: "../images/poori.jpg",
+      name: "poori sagu",
+      category: "breakfast",
+      price: "50₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 5,
+      image: "../images/veg pizza.jpg",
+      name: "veg pizza",
+      category: "lunch",
+      price: "80₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 6,
+      image: "../images/southindian meals.jpg",
+      name: "meals south",
+      category: "dinner",
+      price: "210₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 7,
+      image: "../images/pannerbutter.jpg",
+      name: "panner buttermasala",
+      category: "dinner",
+      price: "150₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 8,
+      image: "../images/nan.jpg",
+      name: "Butter nan",
+      category: "dinner",
+      price: "25₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 9,
+      image: "../images/kaju rice.jpeg",
+      name: "Kaju Rice",
+      category: "lunch",
+      price: "200₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 10,
+      image: "../images/mutton biryani.jpg",
+      name: "mutton biryani",
+      category: "lunch",
+      price: "200₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id:11,
+      image: "../images/strawberry milkshake.jpeg",
+      name: "strawberry milkshake",
+      category: "Beverages",
+      price: "90₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id:12,
+      image: "../images/chocolate milkshake.jpeg",
+      name: "chocalate milkshake",
+      category: "Beverages",
+      price: "100₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id:13,
+      image: "../images/lime soda.jpeg",
+      name: "Lime soda",
+      category: "Beverages",
+      price: "40₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id:14,
+      image: "../images/mojito.jpg",
+      name: "Mojito",
+      category: "Beverages",
+      price: "70₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id:15,
+      image: "../images/orange juice.jpeg",
+      name: "Orange juice",
+      category: "Beverages",
+      price: "50₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id:16,
+      image: "../images/blue lime soda.jpeg",
+      name: "Blue lime juice",
+      category: "Beverages",
+      price: "50₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id:17,
+      image: "../images/watermelon juice.jpeg",
+      name: "watermelon juice",
+      category: "Beverages",
+      price: "50₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id:18,
+      image: "../images/avocado juice.jpeg",
+      name: "Avocada juice",
+      category: "Beverages",
+      price: "80₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id:19,
+      image: "../images/tea.cfe.jpeg",
+      name: "Tea/coffee",
+      category: "Beverages",
+      price: "10/15₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id:20,
+      image: "../images/idli vada.jpg",
+      name: "Idli vada",
+      category: "breakfast",
+      price: "45n₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id:21,
+      image: "../images/Dosa.jpg",
+      name: "Masala Dosa",
+      category: "breakfast",
+      price: "50₹",
+     
+    },
+  ];
+  
+  export default Menu;
